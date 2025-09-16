c1c1f0ae769bfd4774a826ff3cee1b5257eae99c
MD SEFAT AHMED <74113037+Sefat3674@users.noreply.github.com>
2025-09-16 07:48:26 +0600

    Delete library directory

--- library/RecommendationActions.kt
+++
@@ -1,9 +0,0 @@
-package library
-
-import models.Movie
-import models.User
-
-abstract class RecommendationActions {
-    abstract fun recommendMovie(user: User): List<Movie>
-    abstract fun addRating(user: User, movie: Movie, rating: Int)
-}
